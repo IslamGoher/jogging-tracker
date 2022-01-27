3f5952f07d434b0a86a15d758645ccdf60068c82
IslamGoher <sam.m.goher@gmail.com>
2022-01-27 04:25:06 +0200

    Add morgan configs

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import express, { Application } from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import morgan from "morgan";
 
 const app: Application = express();
 const port = process.env.PORT || 3000;
 
-app.listen(app, () => {
+// using morgan only in development mode
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(
     `server is running in ${process.env.NODE_ENV} mode on port ${port}`
