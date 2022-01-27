88ee8b0f6fa1297f50621bff47c5aac22bed15f8
IslamGoher <sam.m.goher@gmail.com>
2022-01-27 04:49:35 +0200

    Add swagger
    - Add swagger dependencies
    - Add swagger configurations

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,22 @@ import express, { Application } from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import morgan from "morgan";
+import * as swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./docs/swagger.json";
 
 const app: Application = express();
 const port = process.env.PORT || 3000;
 
 // using morgan only in development mode
 if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+  // use swagger
+  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  app.use(morgan("dev", {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    skip: (req, res) => {
+      return req.url.startsWith("/docs/");
+    }
+  }));
 }
 
 app.listen(port, () => {
