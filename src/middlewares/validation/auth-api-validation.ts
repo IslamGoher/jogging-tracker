c625cc44864334d6fafd157b4ab433c70030d36b
IslamGoher <sam.m.goher@gmail.com>
2022-01-29 00:14:48 +0200

    Add validation for 'signup' API

---
+++ src/middlewares/validation/auth-api-validation.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express";
+import { ErrorResponse } from "../../util/error-response";
+import Joi from "joi";
+
+export function validateSignup(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const joiSchema = Joi.object({
+      fullname: Joi.string().min(3).max(200).required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().min(8).max(50).required()
+    });
+
+    const result = joiSchema.validate(req.body);
+    
+    if(result.error){
+      const errorMessage = result.error.details[0].message;
+      return next(new ErrorResponse(400, errorMessage));
+    }
+
+    next();
+    
+  } catch (error) {
+    next(error);
+  }
+}
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,10 @@
 import express, { Router } from "express";
 import { postSignup } from "../controllers/auth";
+import { validateSignup } from "../middlewares/validation/auth-api-validation";
 
 export const router: Router = express.Router();
 
 // @route   POST '/api/v1/signup'
 // @desc    Add new user
 // @access  public
-router.post("/signup", postSignup);
+router.post("/signup",validateSignup, postSignup);
