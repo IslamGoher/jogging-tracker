1cad763260af21b2f5979f82a25a720a740289df
IslamGoher <sam.m.goher@gmail.com>
2022-01-29 19:43:06 +0200

    Add authorization role for API 'get jogging'
    - The role doesn't allow manager to access jogging

--- src/routes/jogging.ts
+++ src/routes/jogging.ts
@@ -1,9 +1,10 @@
 import express, { Router } from "express";
 import { getJogging } from "../controllers/jogging";
+import { checkRoleForJoggingApi } from "../middlewares/auth-role-check";
 
 export const router: Router = express.Router();
 
 // @route   GET '/api/v1/jogging'
 // @desc    list all jogging data
 // @access  private (only authorized user can access jogging)
-router.get("/jogging", getJogging);
+router.get("/jogging", checkRoleForJoggingApi, getJogging);
