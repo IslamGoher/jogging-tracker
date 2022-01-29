1d7212599a442e7d52a1c15179bedea4d8e96bbd
IslamGoher <sam.m.goher@gmail.com>
2022-01-30 01:30:08 +0200

    Add validation for API 'get all jogging'

--- src/middlewares/validation/jogging-api-validation.ts
+++ src/middlewares/validation/jogging-api-validation.ts
@@ -8,7 +8,26 @@ export function validateGetJogging(
   next: NextFunction
 ) {
   try {
-    
+    const joiSchema = Joi.object({
+      from: Joi.date().optional(),
+      to: Joi.date().optional(),
+      page: Joi.number().min(1).optional()
+    });
+
+    const object = {
+      from: req.query.from,
+      to: req.query.to,
+      page: req.query.page
+    };
+
+    const result = joiSchema.validate(object);
+
+    if (result.error) {
+      const errorMessage = result.error.details[0].message;
+      return next(new ErrorResponse(400, errorMessage));
+    }
+
+    next();
 
   } catch (error) {
     next(error);
