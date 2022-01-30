b0d62d3c803cfbfa0348c70e8d1d7f510fa4f43c
IslamGoher <sam.m.goher@gmail.com>
2022-01-30 03:46:54 +0200

    Add functionality to API 'get one jogging'

--- src/routes/jogging.ts
+++ src/routes/jogging.ts
@@ -1,5 +1,5 @@
 import express, { Router } from "express";
-import { getJogging } from "../controllers/jogging";
+import { getJogging, getOneJogging } from "../controllers/jogging";
 import { checkRoleForJoggingApi } from "../middlewares/auth-role-check";
 import { validateGetJogging } from "../middlewares/validation/jogging-api-validation";
 
@@ -9,3 +9,8 @@ export const router: Router = express.Router();
 // @desc    list all jogging data
 // @access  private (only authorized user can access jogging)
 router.get("/jogging", checkRoleForJoggingApi, validateGetJogging, getJogging);
+
+// @route   GET '/api/v1/jogging/:id'
+// @desc    get one jogging data by id
+// @access  private (only authorized user can access jogging)
+router.get("/jogging/:id", checkRoleForJoggingApi, getOneJogging);
