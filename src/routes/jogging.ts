61531776dbf2117f196224da935d1cc2f33273bb
IslamGoher <sam.m.goher@gmail.com>
2022-01-30 04:24:09 +0200

    Add validation for API 'get one jogging'

--- src/routes/jogging.ts
+++ src/routes/jogging.ts
@@ -1,7 +1,10 @@
 import express, { Router } from "express";
 import { getJogging, getOneJogging } from "../controllers/jogging";
 import { checkRoleForJoggingApi } from "../middlewares/auth-role-check";
-import { validateGetJogging } from "../middlewares/validation/jogging-api-validation";
+import {
+  validateGetJogging,
+  validateGetOneJogging,
+} from "../middlewares/validation/jogging-api-validation";
 
 export const router: Router = express.Router();
 
@@ -13,4 +16,9 @@ router.get("/jogging", checkRoleForJoggingApi, validateGetJogging, getJogging);
 // @route   GET '/api/v1/jogging/:id'
 // @desc    get one jogging data by id
 // @access  private (only authorized user can access jogging)
-router.get("/jogging/:id", checkRoleForJoggingApi, getOneJogging);
+router.get(
+  "/jogging/:id",
+  checkRoleForJoggingApi,
+  validateGetOneJogging,
+  getOneJogging
+);
