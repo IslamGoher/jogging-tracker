2e986bed7d80c394165f58ba76bf6aa3e532395c
IslamGoher <sam.m.goher@gmail.com>
2022-01-31 11:40:24 +0200

    Add validation for API 'get one user'

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,7 +1,10 @@
 import express, { Router } from "express";
 import { getOneUser, getUsers } from "../controllers/user";
 import { checkRoleForUserApi } from "../middlewares/auth-role-check";
-import { validateGetUsers } from "../middlewares/validation/user-api-validation";
+import {
+  validateGetOneUser,
+  validateGetUsers,
+} from "../middlewares/validation/user-api-validation";
 
 export const router: Router = express.Router();
 
@@ -13,4 +16,4 @@ router.get("/users", checkRoleForUserApi, validateGetUsers, getUsers);
 // @route   GET '/api/v1/users/:id'
 // @desc    list one users data
 // @access  private (only admins and managers can access users data)
-router.get("/users/:id", checkRoleForUserApi, getOneUser);
+router.get("/users/:id", checkRoleForUserApi, validateGetOneUser, getOneUser);
